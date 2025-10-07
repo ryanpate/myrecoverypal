d17499909bdc356e3439406e221351a02cc6e2f7
Ryan Pate <rryan.pate@att.net>
2025-10-06 19:09:24 -0500

    Add rich text editor support for Medium content

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 from .views import (
-    PostListView, PostDetailView, PostCreateView, CategoryListView,
-    TagListView, add_comment
+    PostListView, PostDetailView, PostCreateView, PostUpdateView,
+    CategoryListView, TagListView, MyPostsView, add_comment
 )
 
 app_name = 'blog'
@@ -10,9 +10,10 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('write/', PostCreateView.as_view(), name='post_create'),
+    path('my-posts/', MyPostsView.as_view(), name='my_posts'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category_posts'),
     path('tag/<slug:slug>/', TagListView.as_view(), name='tag_posts'),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<slug:slug>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<slug:slug>/comment/', add_comment, name='add_comment'),
-    path('my-posts/', views.MyPostsView.as_view(), name='my_posts'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -10,15 +10,17 @@ from django.utils import timezone
 from .models import Post, Category, Tag, Comment
 from .forms import CommentForm, PostForm
 
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     paginate_by = 9
-    
+
     def get_queryset(self):
-        queryset = Post.objects.filter(status='published').select_related('author', 'category')
-        
+        queryset = Post.objects.filter(
+            status='published').select_related('author', 'category')
+
         # Search functionality
         search_query = self.request.GET.get('search')
         if search_query:
@@ -27,102 +29,107 @@ class PostListView(ListView):
                 Q(content__icontains=search_query) |
                 Q(excerpt__icontains=search_query)
             )
-        
+
         # Category filter
         category_slug = self.request.GET.get('category')
         if category_slug:
             queryset = queryset.filter(category__slug=category_slug)
-        
+
         # Tag filter
         tag_slug = self.request.GET.get('tag')
         if tag_slug:
             queryset = queryset.filter(tags__slug=tag_slug)
-        
+
         return queryset
-    
-def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context['categories'] = Category.objects.all()
-    context['popular_tags'] = Tag.objects.all()[:15]
-    context['recent_posts'] = Post.objects.filter(
-        status='published'
-    ).order_by('-published_at')[:5]
 
-    # Add stats
-    context['posts_count'] = Post.objects.filter(status='published').count()
-    context['authors_count'] = Post.objects.filter(
-        status='published'
-    ).values('author').distinct().count()
-    context['comments_count'] = Comment.objects.filter(
-        is_approved=True).count()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['popular_tags'] = Tag.objects.all()[:15]
+        context['recent_posts'] = Post.objects.filter(
+            status='published'
+        ).order_by('-published_at')[:5]
 
-    return context
+        # Add stats
+        context['posts_count'] = Post.objects.filter(
+            status='published').count()
+        context['authors_count'] = Post.objects.filter(
+            status='published'
+        ).values('author').distinct().count()
+        context['comments_count'] = Comment.objects.filter(
+            is_approved=True).count()
+
+        return context
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
-    
+
     def get_queryset(self):
         return Post.objects.filter(status='published')
-    
+
     def get_object(self):
         obj = super().get_object()
         # Increment view count
         Post.objects.filter(pk=obj.pk).update(views=F('views') + 1)
         return obj
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         post = self.object
-        
+
         # Get approved comments
         context['comments'] = post.comments.filter(
             is_approved=True, parent=None
         ).select_related('author')
-        
+
         # Get related posts
         context['related_posts'] = Post.objects.filter(
             status='published',
             category=post.category
         ).exclude(pk=post.pk)[:3]
-        
+
         # Comment form
         context['comment_form'] = CommentForm()
-        
+
         return context
+
 
 class CategoryListView(ListView):
     model = Post
     template_name = 'blog/category_posts.html'
     context_object_name = 'posts'
     paginate_by = 12
-    
+
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
         return Post.objects.filter(
             status='published',
             category=self.category
         ).select_related('author')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = self.category
         context['categories'] = Category.objects.all()
         return context
 
+
 class TagListView(ListView):
     model = Post
     template_name = 'blog/tag_posts.html'
     context_object_name = 'posts'
     paginate_by = 12
-    
+
     def get_queryset(self):
         self.tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
         return Post.objects.filter(
             status='published',
             tags=self.tag
         ).select_related('author', 'category')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tag'] = self.tag
@@ -135,6 +142,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:my_posts')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -162,6 +170,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:my_posts')
 
     def test_func(self):
         """Check if user is the author of the post"""
@@ -183,74 +192,32 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         context['button_text'] = 'Update Post'
         return context
 
+
 @login_required
 def add_comment(request, slug):
     post = get_object_or_404(Post, slug=slug, status='published')
-    
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.author = request.user
-            
+
             # Handle reply to comment
             parent_id = request.POST.get('parent_id')
             if parent_id:
                 parent_comment = get_object_or_404(Comment, id=parent_id)
                 comment.parent = parent_comment
-            
+
             comment.save()
-            messages.success(request, 'Your comment has been added successfully!')
-            
+            messages.success(
+                request, 'Your comment has been added successfully!')
+
             if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
                 return JsonResponse({'status': 'success'})
-    
+
     return redirect(post.get_absolute_url())
-class MyPostsView(LoginRequiredMixin, ListView):
-    model = Post
-    template_name = 'blog/my_posts.html'
-    context_object_name = 'posts'
-    paginate_by = 10
-
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user).order_by('-created_at')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['draft_count'] = Post.objects.filter(
-            author=self.request.user,
-            status='draft'
-        ).count()
-        context['published_count'] = Post.objects.filter(
-            author=self.request.user,
-            status='published'
-        ).count()
-        return context
-
-class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author or self.request.user.is_staff
-
-    def form_valid(self, form):
-        if form.instance.status == 'published' and not form.instance.published_at:
-            form.instance.published_at = timezone.now()
-        messages.success(
-            self.request, 'Your post has been updated successfully!')
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Edit Post'
-        context['button_text'] = 'Update Post'
-        return context
 
 
 class MyPostsView(LoginRequiredMixin, ListView):
@@ -280,29 +247,4 @@ class MyPostsView(LoginRequiredMixin, ListView):
             author=self.request.user,
             status='published'
         ).count()
-        return context
-
-
-class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author or self.request.user.is_staff
-
-    def form_valid(self, form):
-        if form.instance.status == 'published' and not form.instance.published_at:
-            form.instance.published_at = timezone.now()
-        messages.success(
-            self.request, 'Your post has been updated successfully!')
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Edit Post'
-        context['button_text'] = 'Update Post'
         return context
